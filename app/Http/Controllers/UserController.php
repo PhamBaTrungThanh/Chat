a281b295624c20326b8c9218c200f7c7c8a808f7
Trung Thành <phambatrungthanh@gmail.com>
2018-05-11 12:38:24 +0700

    add gender supports

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
+use Carbon\Carbon;
 class UserController extends Controller
 {
 
@@ -39,7 +40,41 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        
+        if ($request->filled('wellcome_update')) {
+            $request->validate([
+                "day" => "required_with:month,year|integer",
+                "month" => "required_with:day,year|integer",
+                "year" => "required_with:day,month|integer",
+                "avatar" => "sometimes|nullable|image",
+                "gender" => "required",
+            ]);
+
+            // if user uploaded a file 
+            if ($request->hasFile('avatar')) {
+                // If it valid
+                if ($request->file('avatar')->isValid()) {
+                    
+                    if (env('FILESYSTEM_DRIVER') === 'local') {
+                        $path = $request->file('avatar')->store('public/avatars');
+                        $user->avatar = asset($path);
+                    }
+                }
+            }
+            // if user update their birthday
+            if ($request->filled('day')) {
+                $birthday = Carbon::createFromDate($request->input('year'), $request->input('month'), 1);
+                // compare to day
+                $birth_day = ($birthday->daysInMonth < (int) $request->input('day')) ? $birthday->daysInMonth : $request->input('day');
+                $birthday->day($birth_day);
+                $user->birthday = $birthday;
+            }
+            // set user gender
+            $user->gender = $request->input('gender');
+            $user->save();
+            session()->flash("message", __("Success"));
+        }
+
+        return redirect("/");
     }
 
     /**
--- resources/views/users/wellcome.blade.php
+++ resources/views/users/wellcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="standalone sign-up">
+<div class="standalone wellcome">
     <div class="panel is-responsive is-medium">
         <div class="content">
             <figure class="color-logo">
@@ -12,13 +12,42 @@
                 Rất cám ơn bạn đã đăng ký làm thành viên của <a class="special-link" href="{{env('APP_URL')}}">{{env('APP_NAME')}}</a>.
                 Bạn có vui lòng cung cấp thêm một vài thông tin không?
             </p>
-            <form method="UPDATE" action="{{ route('user.update', auth()->user())}}">
+            <p class="subtitle is-6 is-italic">
+                (Bạn có thể thay đổi thông tin bất cứ lúc nào.)
+            </p>
+            <form method="POST" action="{{ route('user.update', auth()->user())}}" enctype="multipart/form-data">
+                @csrf
+                <input type="hidden" name="_method" value="PUT" />
+                <input type="hidden" name="wellcome_update" value="true" />
                 <div class="field">
+                    <div class="control">
+                        <h4>Giới tính</h4>
+                        <div class="level gender-selector">
+                            <div class="level-item">
+                                <input type="radio" name="gender" value="male" id="gender_male" checked="checked"  class="visually-hidden">
+                                <label for="gender_male" class="choose-gender">
+                                    <figure class="image is-128x128">
+                                        <img src="{{asset('images/boy.png')}}" />
+                                    </figure>
+                                </label>
+                            </div>
+                            <div class="level-item">
+                                <input type="radio" name="gender" value="female" id="gender_female"  class="visually-hidden">
+                                <label for="gender_female" class="choose-gender">
+                                        <figure class="image is-128x128">
+                                            <img src="{{asset('images/girl.png')}}" />
+                                        </figure>
+                                    </label>
+                            </div>
+                        </div>
+                        
+
+                    </div>
                     <div class="control">
                         <h4>Ngày sinh</h4>
                         <b>Ngày</b>
                         <select name="day" id="day">
-                            
+                            <option selected disabled>Chọn ngày</option>
                             @for($i = 1; $i <=31; $i++)
                                 <option value="{{$i}}">{{$i}}</option>
                             @endfor
@@ -27,6 +56,7 @@
                         <b>Tháng</b>
                         <span>&nbsp;</span>
                         <select name="month" id="month">
+                            <option selected disabled>Chọn tháng</option>
                             @for($i = 1; $i <=12; $i++)
                                 <option value="{{$i}}">{{$i}}</option>
                             @endfor
@@ -35,6 +65,7 @@
                         <b>Năm</b>
                         <span>&nbsp;</span>
                         <select name="year" id="year">
+                                <option selected disabled>Chọn năm</option>
                             @for ($i = $now; $i >= $til; $i--)
                                 <option value="{{$i}}">{{$i}}</option>
                             @endfor
@@ -50,6 +81,13 @@
                             {{__("Tải ảnh lên")}}
                         </label>
                     </div>
+                    @if (count($errors->all() > 0))
+                        @foreach($errors->all() as $message)
+                            <p class="help">
+                                {{ $message }}
+                            </p>
+                        @endforeach
+                    @endif
                 </div>
                 
                 <div class="level">
@@ -69,25 +107,7 @@
     </div>
 </div>
 @endsection
-@push('headend')
-    <style>
-        #avatar-image {
-            width: 128px;
-            height: 128px;
-            object-fit: contain;
-            border: 2px solid #ffffff;
-            box-shadow: 0px 0px 0px 1px rgba(85, 85, 85, 0.2);
-            border-radius: 50%;
-            overflow: hidden;
-        }
-        #avatar-image[src=""] {
-            display: none;
-        }
-        .avatar-display {
-            margin: 0 !important;
-        }
-    </style>
-@endpush
+
 @push('bodyend')
     <script language="javascript">
         (function() {
